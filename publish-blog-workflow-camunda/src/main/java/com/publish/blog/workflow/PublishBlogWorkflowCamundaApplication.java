bd84c61f3b7bce59aa7de3460dece85506c451d8
Sunil Bhambani <sunil_bhambani@persistent.co.in>
2019-10-21 11:07:35 +0530

    Implemented positive case for blog publishing use case

--- publish-blog-workflow-camunda/src/main/java/com/publish/blog/workflow/PublishBlogWorkflowCamundaApplication.java
+++ publish-blog-workflow-camunda/src/main/java/com/publish/blog/workflow/PublishBlogWorkflowCamundaApplication.java
@@ -1,13 +1,26 @@
 package com.publish.blog.workflow;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class PublishBlogWorkflowCamundaApplication {
+	
+	@Autowired
+	private RuntimeService runtimeService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PublishBlogWorkflowCamundaApplication.class, args);
 	}
 
+	@EventListener
+	private void processPostDeploy(PostDeployEvent postDeployEvent) {
+		runtimeService.startProcessInstanceByKey("blog_post_submission_process");
+	}
 }
